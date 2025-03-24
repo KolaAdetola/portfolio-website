0441e540f0db07027d07ffdaf0cea3d4e78ddd3e
kolaadetola <kolawoleadetola630@gmail.com>
2025-03-24 20:04:28 +0100

    Update Certications and ProjectCard components to include project links and improve layout

--- src/components/Certications.jsx
+++ src/components/Certications.jsx
@@ -7,32 +7,32 @@ const Certications = () => {
     {
       ImgUrl: "/images/food.jpeg",
       title: "Restaurant Website",
-      tags:["React", "Tailwind CSS"]
+      links:""
     },
     {
       ImgUrl: "/images/github.jpeg",
       title: "Github Profile Clone",
-      tags:["React", "Tailwind CSS"]
+      links:"https://kolaadetola.github.io/responsive-github/"
     },
     {
       ImgUrl: "/images/learnly.jpeg",
-      title: "Project 3",
-      tags:["React", "Tailwind CSS"]
+      title: "Chat/Exam Management system",
+      links:""
     },
     {
       ImgUrl: "/images/quote.jpeg",
-      title: "Project 3",
-      tags:["React", "Tailwind CSS"]
+      title: "quote generator",
+      links:"https://kolaadetola.github.io/Quote-Generator/"
     },
     {
       ImgUrl: "/images/weather.jpeg",
-      title: "Project 3",
-      tags:["React", "Tailwind CSS"]
+      title: "real time weather app",
+      links:"https://kolaadetola.github.io/weather-app/"
     },
     {
-      ImgUrl: "/images/portfolio.jpeg",
-      title: "Project 3",
-      tags:["React", "Tailwind CSS"]
+      ImgUrl: "/images/folio.jpeg",
+      title: "personal portfolio",
+      links:"https://portfolio-website-omega-vert.vercel.app/"
     },
   ]
   return (
@@ -40,7 +40,7 @@ const Certications = () => {
         Projects
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 mt-10 p-4  ">
           {projects.map((project, index) => (
-            <ProjectCard key={index} ImgUrl={project.ImgUrl} title={project.title} description={project.description} tags={project.tags} />
+            <ProjectCard key={index} ImgUrl={project.ImgUrl} title={project.title} description={project.description} link={project.links} />
           ))}
         </div>
     </div>
